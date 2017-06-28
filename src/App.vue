8c3ec5d72a3b6ba38541275351a9415076845308
LiuZiao <burningalive.fff@live.com>
2017-06-28 16:58:58 +0800

    修改了文件夹结构, 基础css

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,40 @@
     margin: 0 auto;
     width: 1030px;
   }
+  * {
+    font: 14px/1.5 "Helvetica Neue", Helvetica, Arial, "Microsoft Yahei", "Hiragino Sans GB", "Heiti SC", "WenQuanYi Micro Hei", sans-serif;
+    text-align: left; // color: #2c3e50;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    list-style-type: none;
+    box-sizing: border-box!important;
+  }
+  html,
+  body {
+    width: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+  a:hover {
+    color: $米橘;
+  }
+  .clearfix::after {
+    content: '';
+    display: block;
+    height: 0;
+    overflow: hidden;
+    clear: both;
+    visibility: hidden;
+  }
 </style>
 
 <template>
   <div id="app">
+
     <router-view></router-view>
+  
   </div>
 </template>
 
