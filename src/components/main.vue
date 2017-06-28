ef766d36aa23784ed6dcf804366467eca1c87e85
LiuZiao <burningalive.fff@live.com>
2017-06-28 11:43:32 +0800

    add: main.vue, del: Hello.vue

---
+++ src/components/main.vue
@@ -0,0 +1,22 @@
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  html, body {
+    width: 100%;
+  }
+</style>
+
+<template>
+  <div class="main">
+    <div class="container">
+      主页
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
